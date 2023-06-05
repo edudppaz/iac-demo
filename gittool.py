f01e332e647919e6639e4dbbb0e877f8ca2a3450
Eduardo Pozo <eduardopozo56@gmail.com>
2023-06-05 23:18:28 +0200

    Testing flask

---
+++ gittool.py
@@ -0,0 +1,71 @@
+import os
+import git
+import random
+import yaml
+from github import Github
+
+def make_pr(yaml_data):
+    # Local repository path
+    repo_path = '/Users/edpozo/Git/iac-demo'
+
+    # File path relative to the repository root
+    file_path = 'README.md'
+
+    # New line to add
+    
+    # Create a new branch
+    repo = git.Repo(repo_path)
+
+    # Checkout the main branch and pull the latest changes
+    print("Checking out main branch")
+    repo.git.checkout('main')
+    print("Getting latest main")
+    repo.git.pull()
+
+    # Create a new branch
+    random_number = random.randint(100, 999)
+    new_branch = f'flask-branch-{random_number}'
+    print("Branching")
+    repo.git.checkout('HEAD', b=new_branch)
+
+    # Modify the file
+    print("Modifying DATA file")
+    with open('napalm/data.yaml', 'r') as file:
+        existing_data = yaml.safe_load(file)
+
+    merged_data = existing_data.copy()
+    for key, value in yaml_data.items():
+        if key in merged_data:
+            merged_data[key].append(value)
+        else:
+            merged_data[key] = value
+
+    with open('napalm/data.yaml', 'w') as file:
+        yaml.dump(merged_data, file)
+
+    # Commit the changes
+    print("Committing")
+    repo.git.add(file_path)
+    repo.git.commit('-m', 'Added a new line to README.md')
+
+    # Push the new branch
+    print("Pushing branch")
+    repo.git.push('--set-upstream', 'origin', new_branch)
+
+    # GitHub credentials
+    github_token = os.getenv("DEMO_TOKEN")
+    repository_name = 'edudppaz/iac-demo'
+    base_branch = 'main'
+
+    # Create a pull request
+    print("New PR")
+    g = Github(github_token)
+    repo = g.get_repo(repository_name)
+    head_branch = f'{repo.owner.login}:{new_branch}'
+    repo.create_pull(title='New Automated Pull Request', body='Please review data changes', base=base_branch, head=head_branch)
+    print("Done")
+
+    # # Clean up - checkout main branch and delete the local branch
+    print("Cleaning up")
+    repo.git.checkout('main')
+    repo.git.branch('-D', new_branch)
---
+++ main_flask.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+# from pypsrp.client import Client
+from flask import Flask, render_template, request, session
+from flask_bootstrap import Bootstrap
+from os import environ
+from gittool import make_pr
+import config
+import yaml
+
+app = Flask(__name__)
+app.config.from_object("config.Config")
+
+Bootstrap(app)
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+    devices = ["R1", "R2"]
+    return render_template("main.html", devices=devices)
+
+
+@app.route("/newvlan", methods=["POST"])
+def newvlan():
+    vlan_name = request.form.get("vlan_name")
+    vlan_id = request.form.get("vlan_id")
+    device = request.form.get("device_choice")
+    yaml_data = {
+        "vrfs" : {
+           "name": vlan_name,
+           "vlan_id": vlan_id,
+           "linknett": f"192.168.{vlan_id}.0",
+           "loopback": f"192.168.{vlan_id}.100"
+        }
+    }
+    make_pr(yaml_data)
+    return render_template(
+        "newvlan.html",
+        vlan_name=vlan_name,
+        vlan_id=vlan_id,
+        device=device,
+    )
+
+if __name__ == "__main__":
+    app.run()
